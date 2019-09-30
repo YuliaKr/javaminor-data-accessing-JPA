dba9aca1cff0de23cd79bf59ea5b5cfdfd77b6b4
Yulia <0937630@hr.nl>
2019-09-30 20:00:03 +0200

    initial commit

---
+++ src/main/java/com/javaminor/accessingDataJPA/DataAccessJPAApplication.java
@@ -0,0 +1,13 @@
+package com.javaminor.accessingDataJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataAccessJPAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataAccessJPAApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javaminor/accessingDataJPA/DataAccessJPAApplicationTests.java
@@ -0,0 +1,15 @@
+package com.javaminor.accessingDataJPA;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DataAccessJPAApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
